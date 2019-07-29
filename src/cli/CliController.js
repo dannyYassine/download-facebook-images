46da03b54d45fd25d18e1fec188381c89f812c20
Danny Yassine <dannyyassine@gmail.com>
2019-07-28 21:32:32 -0400

    controller

---
+++ src/cli/CliController.js
@@ -0,0 +1,51 @@
+require('../bootstrap');
+
+const cliProgress = require('cli-progress');
+
+const { FacebookDownload } = use('@/core/services/FacebookDownload');
+
+class CliController {
+
+  constructor() {
+    const { USER_TOKEN } = process.env;
+    this.facebookDownload = new FacebookDownload(USER_TOKEN);
+    this.cli = new cliProgress.Bar({}, cliProgress.Presets.shades_grey);
+  }
+
+  /**
+   * @returns {Promise<void>}
+   */
+  async start() {
+
+    try {
+      this.facebookDownload.on('album:download:start', album => {
+        console.log(`Album: ${album.name} (${album.photos.length} photos)`);
+        this.cli.start(album.photos.length, 0);
+      });
+
+      this.facebookDownload.on('album:download:end', album => {
+        this.cli.update(album.photos.length);
+        console.log('');
+        this.cli.stop();
+      });
+
+      this.facebookDownload.on('album:download:photo', (photo, index) => {
+        this.cli.update(index+1);
+      });
+
+      this.facebookDownload.on('archive:start', () => {
+        console.log('Archiving...');
+      });
+
+      await this.facebookDownload.downloadUserPhotoAlbums();
+
+      console.log(`Done`);
+    } catch (e) {
+      console.log(`ERROR: ${e.message}`);
+    }
+  }
+}
+
+module.exports = {
+  CliController
+};
--- src/cli/index.js
+++ src/cli/index.js
@@ -1,35 +1,7 @@
 require('../bootstrap');
-
-const cliProgress = require('cli-progress');
-
-const { FacebookDownload } = use('@/core/services/FacebookDownload');
+const { CliController } = require('./CliController');
 
 (async function () {
-
-  const { USER_TOKEN } = process.env;
-  const facebookDownload = new FacebookDownload(USER_TOKEN);
-  const cli = new cliProgress.Bar({}, cliProgress.Presets.shades_grey);
-
-  try {
-    facebookDownload.on('album:download:start', album => {
-      console.log(`Album: ${album.name} (${album.photos.length} photos)`);
-      cli.start(album.photos.length, 0);
-    });
-    facebookDownload.on('album:download:end', album => {
-      cli.update(album.photos.length);
-      console.log('');
-      cli.stop();
-    });
-    facebookDownload.on('album:download:photo', (photo, index) => {
-      cli.update(index+1);
-    });
-    facebookDownload.on('archive:start', () => {
-      console.log('Archiving...');
-    });
-    await facebookDownload.downloadUserPhotoAlbums();
-    console.log(`Done`);
-  } catch (e) {
-    console.log(`ERROR: ${e.message}`);
-  }
-
+  const cliController = new CliController();
+  await cliController.start();
 })();
